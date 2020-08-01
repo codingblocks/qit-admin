bdc4f18d38929acb53b5a15587a30e2c81b04cae
THEjoezack <me@joezack.com>
2020-08-01 18:48:20 -0400

    Adding api project

---
+++ api/src/test/kotlin/net/codingblocks/qit/api/ApiApplicationTests.kt
@@ -0,0 +1,13 @@
+package net.codingblocks.qit.api
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class ApiApplicationTests {
+
+  @Test
+  fun contextLoads() {
+  }
+
+}
